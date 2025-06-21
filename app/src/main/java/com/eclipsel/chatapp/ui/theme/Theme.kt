eb98a1b9612387431323dc5f448a05857cc6af37
Nhsdkk <38581029+Nhsdkk@users.noreply.github.com>
2025-06-21 09:47:52 +0300

    Start adding custom colors

--- app/src/main/java/com/eclipsel/chatapp/ui/theme/Theme.kt
+++ app/src/main/java/com/eclipsel/chatapp/ui/theme/Theme.kt
@@ -1,20 +1,39 @@
 package com.eclipsel.chatapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorScheme = darkColorScheme(
     primary = WhiteSolidPrimary,
     error = RedSolidPrimary,
+    secondary = BlackTransparent20
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = BlackSolidPrimary,
     error = RedSolidPrimary,
+    secondary = WhiteTransparent20
 )
+
+@Composable
+fun extendColor(light: Color, dark: Color, isSystemDarkTheme: Boolean = isSystemInDarkTheme()) : Color {
+    return if (isSystemDarkTheme) dark else light
+}
+
+val ColorScheme.MessageFemale @Composable get() = extendColor(
+    PinkQuaternaryTransparent50, 
+    PinkQuaternaryTransparent50
+);
+
+val ColorScheme.MessageMale @Composable get() = extendColor(
+    BlueSecondaryTransparent50,
+    BlueSecondaryTransparent50
+);
 
 @Composable
 fun ChatAppTheme(
